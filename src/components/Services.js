0e37639d26a5477a0bbdeaf4443ab25dc2d055bc
Nathan Strong <nathanstrong777@gmail.com>
2025-06-25 11:08:18 -0700

    Quick tweak to the services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,13 +12,13 @@ function Services() {
       <div className={gStyles.content}>
         <div className={gStyles.header} style={{ marginBottom: '0.5rem' }}>Services Offered</div>
         We offer homework help as well as catch-up and work-ahead lessons, with flexible scheduling.
-        <br/><br/>
-        We will tutor any level of math between grades 6 and 12. Lower grades may be accepted on a case-by-case basis.
+        <br /><br />
+        We will tutor any level of math between grades 6 and 12. Lower grades may also be accepted on a case-by-case basis.
         <div className={gStyles.header} style={{ marginBottom: '0.5rem', marginTop: '2rem' }}>Recommended Sessions</div>
-        One 2-hour session per week is recommended, though more or less
+        Two 1-hour sessions per week is recommended, though more or less
         can be accommodated according to need.
         <div className={gStyles.header} style={{ marginBottom: '0.5rem', marginTop: '2rem' }}>Pricing</div>
-        A 2-hour session is $60. Other session lengths are at the same rate,
+        A 1-hour session is $30. Other session lengths are at the same rate,
         $30/hr.
       </div>
       <Footer />
